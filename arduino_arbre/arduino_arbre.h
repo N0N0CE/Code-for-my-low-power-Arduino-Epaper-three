48bf1e4cc66959e7e2e2444ba91da1fbdfe3213a
N0N0CE <73484224+N0N0CE@users.noreply.github.com>
2020-10-26 16:33:24 +0100

    Add files via upload

---
+++ arduino_arbre/arduino_arbre.h
@@ -0,0 +1,17 @@
+struct branch;
+struct fork_recursive;
+struct quad_coords;
+struct quad_line;
+
+/*struct draw_recursive {
+  void draw_func_recursive(branch* , int , int , unsigned char, branch*);
+  int branch_time;
+  float growth;
+  float angle;
+  
+  float height;
+  void (*draw_function)(branch*,branch*,int,int,int,int,int,int,char);
+  char color;
+};*/
+
+void setup();
